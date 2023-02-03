108c565c520679d83c9a167771046f31f44547b1
mohit <sankholia.mohit@gmail.com>
2023-02-03 15:32:23 +0530

    scala basics

--- src/main/scala/Basic/hello.scala
+++ src/main/scala/Basic/hello.scala
@@ -1,9 +1,12 @@
 package Basic
 
-object hello {
+object hello extends App {
 
-  def main(args: Array[String]): Unit = {
-    println("Hello World")
-  }
+//  def main(args: Array[String]): Unit = {
+//    println("Hello World")
+//  }
 
+  println("Step 1: How to initialize a String Array with 3 elements")
+  private val array1: Array[String] = Array("Plain Donut", "Strawberry Donut", "Chocolate Donut")
+  println(s"Elements of array1 = ${array1.mkString("/")}")
 }
